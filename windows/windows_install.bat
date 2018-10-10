09b013c119a631a3992b58e3cd0f3188c5baa4f8
capi_x <capi_x@haibane.org>
2018-10-10 12:55:13 +0200

    Add windows support

---
+++ windows/windows_install.bat
@@ -0,0 +1,11 @@
+mkdir F:\GNURadio-3.7\gr-python27\lib\site-packages\aes
+copy ..\python\__init__.py F:\GNURadio-3.7\gr-python27\lib\site-packages\aes
+copy ..\python\aes_crypt_py_bb.py F:\GNURadio-3.7\gr-python27\lib\site-packages\aes
+copy ..\python\aes_decrypt_py_bb.py F:\GNURadio-3.7\gr-python27\lib\site-packages\aes
+copy ..\build\python\*.pyc F:\GNURadio-3.7\gr-python27\lib\site-packages\aes
+copy ..\build\python\*.pyo F:\GNURadio-3.7\gr-python27\lib\site-packages\aes
+
+mkdir F:\GNURadio-3.7\include\aes
+copy ..\include\aes\api.h F:\GNURadio-3.7\include\aes
+
+copy ..\grc\*.xml F:\GNURadio-3.7\share\gnuradio\grc\blocks
